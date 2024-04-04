dd557126b5561a960441b9e03501fb73bfa391fc
joe <brakejoe9@gmail.com>
2024-04-04 08:20:15 +0300

    done

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(sentence => {
+    return sentence.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
 }
